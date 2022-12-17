5e47c3e728e868c9b4eb0093674e93ecf9b763da
thegitparticle <maddamsettysanjit@gmail.com>
2022-12-17 20:21:29 +0530

    add: fetching old jokes

--- components/Home.tsx
+++ components/Home.tsx
@@ -4,6 +4,7 @@ import {
 	useConnect,
 	useContractWrite,
 	usePrepareContractWrite,
+	useContractRead,
 } from "wagmi";
 import { ConnectKitButton } from "connectkit";
 import Jokes from "../utils/Jokes.json";
@@ -13,10 +14,35 @@ export default function Home() {
 
 	const { address } = useAccount();
 
-	const jokesContractConfig = {
+	const { data, isError, isLoading } = useContractRead({
 		address: "0x22d52a1cd58e6ec803ca9B942d70Cd8Db44f08Aa",
 		abi: contractABI,
-	};
+		functionName: "getAllJokes",
+	});
+
+	function RenderOldJokes() {
+		const jokesArray = convertToArray(data);
+
+		if (jokesArray.length > 0) {
+			return (
+				<div className="flex justify-center items-center flex-col">
+					{jokesArray.map((joke, index) => {
+						return (
+							<p className="flex text-off_light my-2" key={index}>
+								{joke.joke}
+							</p>
+						);
+					})}
+				</div>
+			);
+		} else {
+			return (
+				<div className="flex justify-center items-center flex-col">
+					<p className="flex text-off_light my-2">no jokes yet</p>
+				</div>
+			);
+		}
+	}
 
 	const jokeString: string =
 		"Superman once went to a party. Some people wore bitcoin suits, other dogecoin. Superman was upset, no one told him it was a crypto night.";
@@ -29,7 +55,7 @@ export default function Home() {
 		chainId: 5,
 	});
 
-	const { data, isLoading, isSuccess, write } = useContractWrite(config);
+	const { write } = useContractWrite(config);
 
 	return (
 		<div className="flex w-full h-screen flex-col justify-center items-center">
@@ -46,7 +72,21 @@ export default function Home() {
 			>
 				Tell Joke
 			</button>
+			<RenderOldJokes />
 			<ConnectKitButton />
 		</div>
 	);
+}
+
+function convertToArray(val: unknown): any[] {
+	// Use type assertion to tell the TypeScript compiler that val is an array
+	const arr = val as any[];
+
+	// Use Array.isArray() to check if val is actually an array
+	if (Array.isArray(arr)) {
+		return arr;
+	} else {
+		// If val is not an array, return an empty array
+		return [];
+	}
 }
